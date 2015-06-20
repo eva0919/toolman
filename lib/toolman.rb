61c8354342d19826b41e9888da53f5372d88b965
Mikeva <eva091960601@gmail.com>
2015-06-20 22:25:25 +0800

    version 0.1.0 add toolmanS for fork child to be loading bar

--- lib/toolman.rb
+++ lib/toolman.rb
@@ -5,6 +5,10 @@ module Toolman
 		ToolObject.new
 	end
 
+	def self.new_special
+		ToolmanS.new
+	end
+
 	def self.done
 			puts "\n=============================\n   Done!\n   Every thing is good!\n=============================\n"
 	end
@@ -48,4 +52,80 @@ module Toolman
 			@tempPoint = (@tempPoint + 1) % @circleSize
 		end
 	end
+
+	class ToolmanS
+		
+		def initialize
+			@tempPoint = 0 
+			@circle = ["|","\\","-","/"]
+			@circleSize = @circle.size
+			@loadBarPid = 0
+		end
+
+		
+
+		def say(data)
+			puts "\n>>>>>>>>>>>>>>>>>Messange>>>>>>>>>>>>>>>>>"
+			puts "#{data}"
+			puts ">>>>>>>>>>>>>>>>>Messange>>>>>>>>>>>>>>>>>\n"
+		end
+
+		def loading_bar(nowPoint,endPoint)
+			read, write = IO.pipe
+			@loadBarPid = fork {
+					
+				
+				write.close
+				continue = true
+				sleep_time = 1.0/7.0
+				index = 0 
+				while(continue) do
+					p = (( (nowPoint*1.0) / endPoint) * 100).floor
+					print "\r"
+					print "                                                  \r"
+					print "%5d%s%3s%-3s" % [p,"%","#{@circle[@tempPoint]}",""]
+					@tempPoint = (@tempPoint + 1) % @circleSize
+					if p == 100
+						print "\r"
+						print "                                                  \r"
+						print "%5d%s%3s%-3s" % [p,"%","",""]
+						read.close
+						exit(0)
+					end
+					sleep(sleep_time)
+					s = IO.select([read],[],[],sleep_time)
+					if s 
+						result = read.read(1)
+						if result
+							if result.to_i == 1
+								nowPoint += 1
+							elsif result.to_i == 0
+								puts ""
+								puts "Early break..."
+								read.close
+								exit(0)
+							end
+						end
+						index += 1
+					end
+
+					
+				end
+
+			}
+			read.close
+			@write = write
+			# Process.wait(@loadBarPid)
+			# puts "hi"
+		end
+
+		def loading_bar_add()
+			@write.write(1)
+		end
+
+		def loading_bar_end()
+			@write.write(0)
+		end
+	end
+
 end
